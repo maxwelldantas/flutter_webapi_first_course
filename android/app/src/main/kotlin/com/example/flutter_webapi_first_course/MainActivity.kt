8810ee0a3e1fa052f59841533a9dd0b03149f1a3
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-08-07 23:18:20 -0300

    Projeto iniciado

---
+++ android/app/src/main/kotlin/com/example/flutter_webapi_first_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_webapi_first_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {"home": (context) => const HomeScreen()},
+    );
+  }
+}
